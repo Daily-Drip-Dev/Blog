9622916553a1ce07e89f8f9e96b88a685677192b
Kim Da Eun <midekuna@gmail.com>
2024-11-25 13:52:38 +0900

    feat: initial setting

---
+++ pages/_meta.js
@@ -0,0 +1,8 @@
+// It tells Nextra the order of each page, and the correct title.
+export default {
+  index: 'Home',
+  // Alternatively, you can do it with title property and have other configurations in there as well:
+  // about: {
+  //   title: 'About Us'
+  // }
+};
